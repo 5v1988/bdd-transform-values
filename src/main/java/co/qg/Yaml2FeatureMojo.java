aca7258b74d48bd208444e51ce492b502c04b9ef
Veera N <nmani0303@gmail.com>
2023-05-01 13:20:57 +0800

    Support for both yaml and json files

--- src/main/java/co/qg/Yaml2FeatureMojo.java
+++ src/main/java/co/qg/Yaml2FeatureMojo.java
@@ -1,5 +1,6 @@
 package co.qg;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,6 +11,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,29 +31,37 @@ import org.yaml.snakeyaml.Yaml;
 public class Yaml2FeatureMojo extends AbstractMojo {
 
   public static String FEATURE_FILE_EXT = ".feature";
-  public static String YAML_FILE_EXT = ".yaml";
-  public static String YML_FILE_EXT = ".yml";
 
-  @Parameter(defaultValue = "${project.build.directory}", required = true)
+  @Parameter(defaultValue = "${project.build.directory}", required = true, readonly = true)
   private File target;
+
+  @Parameter(property = "substitute-data.tokenFileName", defaultValue = "token.yaml")
+  private String tokenFileName;
+
+  @Parameter(property = "substitute-data.tokenFileFormat", defaultValue = "yaml")
+  private String tokenFileFormat;
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     getLog().info("Begins substituting all tokenized values in feature files!");
-
     try {
-      Stream<Path> allYaml = Files.walk(Paths.get(target.getAbsolutePath())).filter(yaml ->
-          yaml.toFile().getAbsolutePath().trim().endsWith(YAML_FILE_EXT) |
-              yaml.toFile().getAbsolutePath().trim().endsWith(YML_FILE_EXT));
-      String ymlName = allYaml.collect(Collectors.toList()).get(0).toFile().getAbsolutePath();
-      JSONObject yamlObj = new JSONObject(getYamlObject(ymlName));
-      getLog().info(String.format("Picking up Yaml file: %s for the replacing values", ymlName));
+      Stream<Path> tokenFiles = Files.walk(Paths.get(target.getAbsolutePath())).filter(tokenFile ->
+          tokenFile.getFileName().toString().equalsIgnoreCase(tokenFileName));
+      List<Path> fileList = tokenFiles.collect(Collectors.toList());
+      if (fileList.size() != 1) {
+        throw new AssertionError(
+            String.format("Token file: [ %s ] is not in the project", tokenFileName));
+      }
+      String tokenFile = fileList.get(0).toFile().getAbsolutePath();
+      JSONObject tokenObj = new JSONObject(getTokensAsObject(tokenFile, tokenFileFormat));
 
+      getLog().info(String.format("Picking up token file: %s for the replacing values", tokenFile));
       Stream<Path> features = Files.walk(Paths.get(target.getAbsolutePath())).filter(feature ->
           feature.toFile().getAbsolutePath().trim().endsWith(FEATURE_FILE_EXT));
 
       features.forEach(feature -> {
-        getLog().info(String.format("Processing the feature file: %s", feature.toFile().getAbsolutePath()));
+        getLog().info(
+            String.format("Processing the feature file: %s", feature.toFile().getAbsolutePath()));
         try {
           Charset charset = StandardCharsets.UTF_8;
           String content = null;
@@ -59,13 +70,13 @@ public class Yaml2FeatureMojo extends AbstractMojo {
           Matcher m = Pattern.compile("(\\(.*\\))").matcher(content);
           while (m.find()) {
             tokenToReplace = m.group();
-            tokenJp = m.group().replace("(", "").replace(")", "");
-            String tokenReplaced = JsonPath.read(yamlObj.toString(), "$." + tokenJp);
+            tokenJp = m.group().replaceAll("\\(|\\)", "");
+            String tokenReplaced = JsonPath.read(tokenObj.toString(), "$." + tokenJp);
             content = content.replace(tokenToReplace, tokenReplaced);
-            getLog().info(String.format("Replaced Token: %s", tokenToReplace));
-            getLog().info(String.format("Replacing Token: %s", tokenReplaced));
           }
           Files.write(feature, content.getBytes(charset));
+          getLog().info(String.format("Completed processing the feature file: %s",
+              feature.toFile().getAbsolutePath()));
         } catch (IOException io) {
           getLog().error(io.getMessage());
         }
@@ -76,13 +87,18 @@ public class Yaml2FeatureMojo extends AbstractMojo {
 
   }
 
-  private static Map<String, Object> getYamlObject(String yamlName) throws IOException {
+  private static Map<String, Object> getTokensAsObject(String fileName, String fileFormat)
+      throws IOException {
     Map<String, Object> data;
-    try (InputStream inputStream = new FileInputStream(yamlName)) {
-      Yaml yaml = new Yaml();
-      data = yaml.load(inputStream);
+    try (InputStream inputStream = new FileInputStream(fileName)) {
+      if (fileFormat.equalsIgnoreCase("json")) {
+        data = new ObjectMapper().readValue(new File(fileName), HashMap.class);
+      } else {
+        Yaml yaml = new Yaml();
+        data = yaml.load(inputStream);
+      }
     } catch (IOException e) {
-      throw new IOException(" Exception reading Yaml object!");
+      throw new IOException(String.format(" Exception reading token object : %s", e.getMessage()));
     }
     return data;
   }
